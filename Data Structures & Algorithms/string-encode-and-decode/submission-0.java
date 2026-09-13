a186d10204d276494510387a251dc1fe7e0d639a
Adrian Alemany <adrian.alemany2@gmail.com>
2026-09-13 10:20:08 -0700

    Add: string-encode-and-decode - submission-0

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-0.java
@@ -0,0 +1,31 @@
+class Solution {
+
+    public String encode(List<String> strs) {
+        StringBuilder encoded_string = new StringBuilder();
+
+        for (int i = 0; i < strs.size(); i++) {
+            encoded_string.append(strs.get(i).length());
+            encoded_string.append("#");
+            encoded_string.append(strs.get(i));
+        }
+
+        return encoded_string.toString();
+    }
+
+    public List<String> decode(String str) {
+        int pos = 0;
+        ArrayList<String> decoded_strs = new ArrayList<>();
+
+        while (pos < str.length()) {
+            int hashIndex = str.indexOf('#', pos);
+            int length = Integer.parseInt(str.substring(pos, hashIndex));
+            
+            String word = str.substring(hashIndex + 1, hashIndex + 1 + length);
+            decoded_strs.add(word);
+            pos = hashIndex + 1 + length;
+        }
+
+
+        return decoded_strs;
+    }
+}
